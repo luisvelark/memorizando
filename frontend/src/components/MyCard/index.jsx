61df514204beda95c355f187abe866a11dddccd7
luisvelark <velark07@gmail.com>
2023-03-09 19:38:21 -0300

    feat(front):tablero de cards funcionando

--- frontend/src/components/MyCard/index.jsx
+++ frontend/src/components/MyCard/index.jsx
@@ -1,10 +1,12 @@
-import { useState } from "react";
+import { useState, useEffect } from "react";
 import { styled } from "@mui/material/styles";
+import ReactCardFlip from "react-card-flip";
+import backFace from "../../assets/images/pregunta.jpg";
 
 import { Card, Box, CardContent, Typography } from "@mui/material/";
 
 const Caja = styled(Card)`
-  background-color: blue;
+  background-color: gris;
   height: 120px;
   min-width: 120px;
   cursor: pointer;
@@ -13,25 +15,61 @@ const Caja = styled(Card)`
   }
 `;
 
-export const MyCard = ({ nombre }) => {
-  // const [count, setCount] = useState(0);
+const Img = styled("img")`
+  height: 120px;
+  width: 100%;
+  min-width: 120px;
+`;
+
+export const MyCard = ({
+  name,
+  number,
+  frontFace,
+  flipCard,
+  unflippedCards,
+  disabledCards,
+}) => {
+  const [isFlipped, setIsFlipped] = useState(false);
+  const [hasEvent, setHasEvent] = useState(true);
+
+  useEffect(() => {
+    if (unflippedCards.includes(number)) {
+      setTimeout(() => setIsFlipped(false), 1200);
+      console.log("voltear");
+    }
+  }, [unflippedCards]);
+
+  useEffect(() => {
+    if (disabledCards.includes(number)) {
+      setHasEvent(false);
+    }
+  }, [disabledCards]);
+
+  const handleClick = (e) => {
+    console.log(name, number);
+    const value = flipCard(name, number);
+    if (value !== 0) {
+      setIsFlipped(!isFlipped);
+      console.log("clickeando la otra");
+    } else {
+      console.log("clickeando la misma");
+    }
+  };
 
   return (
     <Caja>
-      {/* <CardContent>
-        <Typography sx={{ fontSize: 14 }} color="text.secondary" gutterBottom>
-          Word of the Day
-        </Typography>
-        <Typography sx={{ mb: 1.5 }} color="text.secondary">
-          adjective
-        </Typography>
-        <Typography variant="body2">
-          well meaning and kindly.
-          <br />
-          {'"a benevolent smile"'}
-        </Typography>
-      </CardContent> */}
-      <p>soy carta</p>
+      <ReactCardFlip isFlipped={isFlipped}>
+        <Img
+          src={backFace}
+          alt="back-face"
+          onClick={hasEvent ? handleClick : null}
+        />
+        <Img
+          src={frontFace}
+          alt="front-face"
+          onClick={hasEvent ? handleClick : null}
+        />
+      </ReactCardFlip>
     </Caja>
   );
 };
--- frontend/src/components/Tablero/index.jsx
+++ frontend/src/components/Tablero/index.jsx
@@ -8,10 +8,66 @@ import { deportes } from "../../const/imagenes";
 
 export const Tablero = () => {
   const [cards, setCards] = useState([]);
+  const [firstCard, setFirstCard] = useState({});
+  const [secondCard, setSecondCard] = useState({});
+  const [unflippedCards, setUnflippedCards] = useState([]); //arreglo de cartas que vuelve a su estado normal
+  const [disabledCards, setDisabledCards] = useState([]); //arreglo de cartas macheadas
 
   useEffect(() => {
+    // AleatoryCards(deportes);
     setCards(deportes);
   }, []);
+
+  useEffect(() => {
+    checkForMatch();
+  }, [secondCard]);
+
+  const AleatoryCards = (array) => {
+    for (let i = array.length - 1; i > 0; i--) {
+      let j = Math.floor(Math.random() * (i + 1));
+      let temp = array[i];
+      array[i] = array[j];
+      array[j] = temp;
+    }
+  };
+
+  const flipCard = (name, number) => {
+    if (firstCard.name === name && firstCard.number === number) {
+      console.log(firstCard, secondCard);
+      return 0; //para no voltear la misma carta!
+    }
+
+    if (!firstCard.name) {
+      setFirstCard({ name, number });
+    } else if (!secondCard.name) {
+      setSecondCard({ name, number });
+    }
+
+    return 1;
+  };
+
+  const checkForMatch = () => {
+    if (firstCard.name && secondCard.name) {
+      const match = firstCard.name === secondCard.name;
+      match ? disableCards() : unflipCards();
+    }
+  };
+
+  const disableCards = () => {
+    setDisabledCards([firstCard.number, secondCard.number]);
+    resetCards();
+  };
+
+  // cartas que se van a voltear
+  const unflipCards = () => {
+    setUnflippedCards([firstCard.number, secondCard.number]);
+    resetCards();
+  };
+
+  const resetCards = () => {
+    setFirstCard({});
+    setSecondCard({});
+  };
 
   return (
     <Grid
@@ -22,9 +78,16 @@ export const Tablero = () => {
       spacing={{ xs: 2, md: 2 }}
       // columns={{ xs: 2, sm: 12, md: 8 }}
     >
-      {cards.map((dato) => (
-        <Grid item xs={8} sm={6} md={4} lg={3} key={dato.id}>
-          <MyCard nombre={dato.nombre} />
+      {cards.map((dato, index) => (
+        <Grid item xs={8} sm={6} md={4} lg={3} key={index}>
+          <MyCard
+            name={dato.name}
+            number={index}
+            frontFace={dato.url}
+            flipCard={flipCard}
+            unflippedCards={unflippedCards}
+            disabledCards={disabledCards}
+          />
         </Grid>
       ))}
     </Grid>
